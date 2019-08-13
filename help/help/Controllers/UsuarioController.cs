18138449b35842d45e5e069945a97dab3c8a5e6b
RobsonJunior1994 <robsonjunior1994@gmail.com>
2019-08-13 14:04:16 -0300

    Primeiro Commit Atualizando os arquivos de bootstrap e jquery e criando a classe de usuario e sua validação

---
+++ help/help/Controllers/UsuarioController.cs
@@ -0,0 +1,26 @@
+﻿using help.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace help.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ help/help/Models/Usuario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace help.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string RepeteSenha { get; set; }
+
+
+        public bool ehValido()
+        {
+            if (string.IsNullOrWhiteSpace(Nome) || string.IsNullOrWhiteSpace(Sobrenome) || string.IsNullOrWhiteSpace(Email) ||
+                string.IsNullOrWhiteSpace(Senha) || string.IsNullOrWhiteSpace(RepeteSenha) || Nome.Any(x => char.IsDigit(x)) || 
+                Sobrenome.Any(x => char.IsDigit(x)) || Senha.Length < 8 || Senha != RepeteSenha)
+            {
+                return false;
+
+            } else
+            {
+                return true;
+            }
+        }
+    }
+}
