30fe96a3298a8a26395f1b74878d1c3b5150416c
RobsonJunior1994 <robsonjunior1994@gmail.com>
2019-08-13 17:51:04 -0300

    Criando interface,repository de usuario e baixando o ninject

---
+++ help/help/Models/IUsuarioRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace help.Models
+{
+    interface IUsuarioRepository
+    {
+        void Salvar(Usuario usuario);
+    }
+}
---
+++ help/help/Repository/UsuarioRepository.cs
@@ -0,0 +1,17 @@
+﻿using help.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace help.Repository
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private HelpDbContext _context;
+        public void Salvar(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
